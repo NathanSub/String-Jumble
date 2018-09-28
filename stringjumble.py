f92fced73c497d9c498135584d6640b78efa32b0
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-09-28 12:52:33 -0400

    Updated from Brython Server: 9/28/2018 12:52:32 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,5 +26,6 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 s = input("Please enter a string of text (the bigger the better): ")
+print(s[::-1])
 wlist = s.split()
 print(wlist)
